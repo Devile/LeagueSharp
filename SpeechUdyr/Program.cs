8fc0339744672a7eea942daf9fc92230a8a27453
Devile <sahino1996@hotmail.de>
2014-11-12 10:59:50 +0100

    Deleted Summoners from SpeechUdyr, use SpeechSummoners

--- SpeechUdyr/Program.cs
+++ SpeechUdyr/Program.cs
@@ -25,21 +25,10 @@ namespace SpeechUdyr
         static Spell W = new Spell(SpellSlot.W);
         static Spell E = new Spell(SpellSlot.E);
         static Spell R = new Spell(SpellSlot.R);
-        static Spell C = new Spell(SpellSlot.Recall);
-        static SpellSlot F;
-        static SpellSlot I;
-        static SpellSlot S;
-        static SpellSlot H;
-        static SpellSlot G;
-        static SpellSlot T;
 
         static Timer speechTimer;
         static bool wantSpeech;
         static int oldInterval;
-
-        static Obj_AI_Base _selectedSmiteTarget;
-        static Obj_AI_Base _selectedIgniteTarget;
-        static Obj_AI_Base _selectedTeleportTarget;
 
         static void Main(string[] args)
         {
@@ -47,22 +36,14 @@ namespace SpeechUdyr
 
             Game.PrintChat("<font color='#FFFFCC'>UdyrSpeech</font> - by Maufeat");
 
-            speechList.Add(new string[] { "tiger", "teiger", "turtle", "bear", "phoenix", "recall", "beer", "flash", "smite", "ghost", "ignite", "tele", "teleport"});
+            speechList.Add(new string[] { "tiger", "teiger", "turtle", "bear", "phoenix", "beer"});
             gr = new Grammar(new GrammarBuilder(speechList));
-
             
             menu = new Menu("Udyr Speech", "udyr", true);
             menu.AddSubMenu(new Menu("Enable", "enable"));
             menu.SubMenu("enable").AddItem(new MenuItem("speech", "enable").SetValue(true));
             menu.SubMenu("enable").AddItem(new MenuItem("speechinterval", "delay").SetValue(new Slider(1000, 250, 5000)));
             menu.AddToMainMenu();
-
-            F = ObjectManager.Player.GetSpellSlot("SummonerFlash");
-            I = ObjectManager.Player.GetSpellSlot("SummonerIgnite");
-            H = ObjectManager.Player.GetSpellSlot("SummonerHeal");
-            G = ObjectManager.Player.GetSpellSlot("SummonerHaste");
-            S = ObjectManager.Player.GetSpellSlot("SummonerSmite");
-            T = ObjectManager.Player.GetSpellSlot("SummonerTeleport");
 
             speechTimer = new Timer(TimerCallBack, null, 0, menu.Item("speechinterval").GetValue<Slider>().Value);
             Game.OnGameUpdate += Game_OnGameUpdate;
@@ -125,102 +106,6 @@ namespace SpeechUdyr
                 case "phoenix":
                     R.Cast();
                     break;
-                case "recall":
-                    C.Cast();
-                    break;
-                case "flash":
-                    if(!IsReadyFlash())
-                    {
-                        Message("Flash not ready!");
-                        return;
-                    }
-                    Vector2 maxRange = ObjectManager.Player.ServerPosition.To2D().Extend(Game.CursorPos.To2D(), 400);
-                    Vector3 position = maxRange.To3D();
-                    ObjectManager.Player.SummonerSpellbook.CastSpell(F, position);
-                    break;
-                case "smite":
-                    if (!IsReadySmite())
-                    {
-                        Message("Smite not ready!");
-                        return;
-                    }
-                    _selectedSmiteTarget = null;
-                    foreach (var enemy in
-                        ObjectManager.Get<Obj_AI_Minion>()
-                            .Where(minion => minion.Team == GameObjectTeam.Neutral)
-                            .OrderByDescending(m => m.Distance(Game.CursorPos))
-                            .Where(target => target.Distance(Game.CursorPos) < 200))
-                    {
-                        _selectedSmiteTarget = enemy;
-                    }
-                    if (_selectedSmiteTarget != null)
-                    {
-                        ObjectManager.Player.SummonerSpellbook.CastSpell(S, _selectedSmiteTarget);
-                        _selectedSmiteTarget = null;
-                    }
-                    else if (_selectedSmiteTarget == null)
-                    {
-                        Message("No Smite Target!");
-                    }
-                    break;
-                case "ignite":
-                    if(!IsReadyIgnite())
-                    {
-                        Message("Ignite not ready!");
-                        return;
-                    }
-                    _selectedIgniteTarget = null;
-                    foreach (var enemy in
-                        ObjectManager.Get<Obj_AI_Hero>()
-                            .Where(minion => minion.IsEnemy)
-                            .OrderByDescending(m => m.Distance(Game.CursorPos))
-                            .Where(target => target.Distance(Game.CursorPos) < 200))
-                    {
-                        _selectedIgniteTarget = enemy;
-                    }
-                    if (_selectedIgniteTarget != null)
-                    {
-                        ObjectManager.Player.SummonerSpellbook.CastSpell(I, _selectedIgniteTarget);
-                        _selectedIgniteTarget = null;
-                    }
-                    else if (_selectedIgniteTarget == null)
-                    {
-                        Message("No Smite Target!");
-                    }
-                    break;
-                case "ghost":
-                    if (!IsReadyGhost())
-                    {
-                        Message("Ghost not ready!");
-                        return;
-                    }
-                    ObjectManager.Player.SummonerSpellbook.CastSpell(G);
-                    break;
-                case "teleport":
-                    if (!IsReadyTeleport())
-                    {
-                        Message("Teleport not ready!");
-                        return;
-                    }
-                    _selectedTeleportTarget = null;
-                    foreach (var enemy in
-                        ObjectManager.Get<Obj_AI_Turret>()
-                            .Where(minion => minion.Health > 0.1)
-                            .OrderByDescending(m => m.Distance(Game.CursorPos))
-                            .Where(target => target.Distance(Game.CursorPos) < 200))
-                    {
-                        _selectedTeleportTarget = enemy;
-                    }
-                    if (_selectedTeleportTarget != null)
-                    {
-                        ObjectManager.Player.SummonerSpellbook.CastSpell(T, _selectedTeleportTarget);
-                        _selectedTeleportTarget = null;
-                    }
-                    else if (_selectedIgniteTarget == null)
-                    {
-                        Message("No Teleport Target!");
-                    }
-                    break;
                 default:
                     Game.PrintChat(e.Result.Text);
                     break;
@@ -229,26 +114,6 @@ namespace SpeechUdyr
         static void Message(string msg)
         {
             Game.PrintChat("<font color='#00FF00'>" + ObjectManager.Player.Name + ":</font> " + msg);
-        }
-        static bool IsReadyFlash()
-        {
-            return (F != SpellSlot.Unknown && ObjectManager.Player.SummonerSpellbook.CanUseSpell(F) == SpellState.Ready);
-        }
-        static bool IsReadySmite()
-        {
-            return (S != SpellSlot.Unknown && ObjectManager.Player.SummonerSpellbook.CanUseSpell(S) == SpellState.Ready);
-        }
-        static bool IsReadyGhost()
-        {
-            return (G != SpellSlot.Unknown && ObjectManager.Player.SummonerSpellbook.CanUseSpell(G) == SpellState.Ready);
-        }
-        static bool IsReadyIgnite()
-        {
-            return (I != SpellSlot.Unknown && ObjectManager.Player.SummonerSpellbook.CanUseSpell(I) == SpellState.Ready);
-        }
-        static bool IsReadyTeleport()
-        {
-            return (T != SpellSlot.Unknown && ObjectManager.Player.SummonerSpellbook.CanUseSpell(T) == SpellState.Ready);
         }
     }
 }
